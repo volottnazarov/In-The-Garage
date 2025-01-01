21bba870d9f65806ea41888eddee7418b84ecbe7
владимир назаров <volottnazarov@mail.ru>
2025-01-02 01:11:27 +0300

    Приложение гараж

---
+++ Garage/data.js
@@ -0,0 +1,38 @@
+﻿const dataJSON = `[
+    {
+        "id": 1,
+        "model": "Гранта"
+        "gosNumber": 6004,
+        "invNumber": 000133,
+        "vinNumber": "XTA21090123456789",
+        "garageNumber": 1
+    },{
+        "id": 2,
+        "model": "Гранта"
+        "gosNumber": 6204,
+        "invNumber": 000234,
+        "vinNumber": "XTA21090123156777",
+        "garageNumber": 2
+    },{
+        "id": 3,
+        "model": "Гранта"
+        "gosNumber": 6302,
+        "invNumber": 000635,
+        "vinNumber": "XTA21090123226789",
+        "garageNumber": 3
+    },{
+        "id": 4,
+        "model": "Гранта"
+        "gosNumber": 6408,
+        "invNumber": 020732,
+        "vinNumber": "XTA21090123450089",
+        "garageNumber": 4
+    },{
+        "id": 5,
+        "model": "Гранта"
+        "gosNumber": 6504,
+        "invNumber": 002239,
+        "vinNumber": "XTA21090123456700",
+        "garageNumber": 5
+    }
+]`
---
+++ Garage/index.js
@@ -0,0 +1,1 @@
+﻿const data = JSON.parse(dataJSON);
