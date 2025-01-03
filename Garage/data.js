858bd1e4fe330269d0a84097730ad2951b2667b1
владимир назаров <volottnazarov@mail.ru>
2025-01-04 00:54:20 +0300

    Справочник по автопарку

--- Garage/data.js
+++ Garage/data.js
@@ -1,38 +1,62 @@
-﻿const dataJSON = `[
+﻿const garageData = [
     {
-        "id": 1,
-        "model": "Гранта"
-        "gosNumber": 6004,
-        "invNumber": 000133,
-        "vinNumber": "XTA21090123456789",
-        "garageNumber": 1
-    },{
-        "id": 2,
-        "model": "Гранта"
-        "gosNumber": 6204,
-        "invNumber": 000234,
-        "vinNumber": "XTA21090123156777",
-        "garageNumber": 2
-    },{
-        "id": 3,
-        "model": "Гранта"
-        "gosNumber": 6302,
-        "invNumber": 000635,
-        "vinNumber": "XTA21090123226789",
-        "garageNumber": 3
-    },{
-        "id": 4,
-        "model": "Гранта"
-        "gosNumber": 6408,
-        "invNumber": 020732,
-        "vinNumber": "XTA21090123450089",
-        "garageNumber": 4
-    },{
-        "id": 5,
-        "model": "Гранта"
-        "gosNumber": 6504,
-        "invNumber": 002239,
-        "vinNumber": "XTA21090123456700",
-        "garageNumber": 5
+        id: 1,
+        model: "ГАЗ-32213",
+        gosNumber: 6325,
+        invNumber: "35077",
+        vinNumber: "X9632210D0753709",
+        release: 2013,
+        garageNumber: "ангар",
+        service: "АДПЛС"
+    },
+    {
+        id: 2,
+        model: "Гранта-2190",
+        gosNumber: 6094,
+        invNumber: "0279",
+        vinNumber: "XTA210940R1067770",
+        release: 2024,
+        garageNumber: 2,
+        service: "АП"
+    },
+    {
+        id: 3,
+        model: "Гранта-2190",
+        gosNumber: 6456,
+        invNumber: "35632",
+        vinNumber: "XTA210910M0729056",
+        release: 2020,
+        garageNumber: 3,
+        service: "АП ГЗ"
+    },
+    {
+        id: 4,
+        model: "Гранта-2190",
+        gosNumber: 6054,
+        invNumber: "35594",
+        vinNumber: "XTA210910L0676731",
+        release: 2020,
+        garageNumber: 4,
+        service: "АП ГЗ"
+    },
+    {
+        id: 5,
+        model: "Гранта-2190",
+        gosNumber: 6055,
+        invNumber: "35555",
+        vinNumber: "XTA210910K0632524",
+        release: 2019,
+        garageNumber: 5,
+        service: "АП ГЗ"
+    },
+    {
+        id: 6,
+        model: "Гранта-2190",
+        gosNumber: 5792,
+        invNumber: "35554",
+        vinNumber: "XTA219010K06322616",
+        release: 2019,
+        garageNumber: 6,
+        service: "АП ГЗ"
     }
-]`
+]
--- Garage/index.js
+++ Garage/index.js
@@ -1,1 +1,109 @@
-﻿const data = JSON.parse(dataJSON);
+﻿const selectEl = document.querySelector(".mySelect");
+const inputElem = document.querySelector('.input');
+
+selectEl.addEventListener("change", () => {
+    const valueEl = selectEl.value;
+    switch (valueEl) {
+        case "gosNumber":
+            let inputEl = document.querySelector('.inputPlace');
+            let inputButtonEl = document.querySelector('.inputButton');
+            let param;
+            let gosNumber = "gosNumber";
+            inputEl.placeholder = "6002 (пример)";
+            inputEl.style.display = 'block';
+            inputButtonEl.style.display = 'block';
+            inputButtonEl.onclick = function() {
+                param = inputEl.value;
+                if(param.length === 4 || param === ""){
+                    viewResult(param, gosNumber);
+                }else{
+                    alert("Гос. номер должен состоять из 4 цифр!")
+                }
+            }
+            break;
+        case "invNumber":
+            let inputInvEl = document.querySelector('.inputPlace');
+            let inputButtonInvEl = document.querySelector('.inputButton');
+            let paramInv;
+            let invNumber = "invNumber";
+            inputInvEl.placeholder = "000234 (пример)";
+            inputInvEl.style.display = 'block';
+            inputButtonInvEl.style.display = 'block';
+            inputButtonInvEl.onclick = function() {
+                paramInv = inputInvEl.value;
+                if(paramInv.length === 5 || paramInv.length === 4 || paramInv === ""){
+                    viewResult(paramInv, invNumber);
+                }else{
+                    alert("Инв. номер должен состоять из 4-5 символов!")
+                }
+            }
+            break;
+        case "vinNumber":
+            let inputVinEl = document.querySelector('.inputPlace');
+            let inputButtonVinEl = document.querySelector('.inputButton');
+            let paramVin;
+            let vinNumber = "vinNumber";
+            inputVinEl.placeholder = "17 знаков!";
+            inputVinEl.style.display = 'block';
+            inputButtonVinEl.style.display = 'block';
+            inputButtonVinEl.onclick = function() {
+                paramVin = inputVinEl.value;
+                if(paramVin.length === 17 || paramVin === ""){
+                    viewResult(paramVin, vinNumber);
+                }else{
+                    alert("VIN номер должен состоять из 17 символов!")
+                }
+            }
+            break;
+        case "garageNumber":
+            let inputGarageEl = document.querySelector('.inputPlace');
+            let inputButtonGarageEl = document.querySelector('.inputButton');
+            let paramGarage;
+            let garageNumber = "garageNumber";
+            inputGarageEl.placeholder = "От 1 до 5";
+            inputGarageEl.style.display = 'block';
+            inputButtonGarageEl.style.display = 'block';
+            inputButtonGarageEl.onclick = function() {
+                paramGarage = inputGarageEl.value;
+                if(paramGarage <= 5 || paramGarage == "ангар"){
+                    viewResult(paramGarage, garageNumber);
+                }else{
+                    alert("Всего 40 гаражей!")
+                }
+            }
+            break;
+        default:
+            break;
+    }
+});
+
+function viewResult(params, category) {
+    const resultList = document.getElementById("resultList");
+    resultList.innerHTML = "";
+
+    garageData.forEach((auto) => {
+        console.log(params, auto[category]);
+        if (params === "" || auto[category] === +params || auto[category] == params) {
+            const modelItem = document.createElement('li');
+            modelItem.textContent = `Модель: ${auto.model}`;
+            const gosItem = document.createElement('li');
+            gosItem.textContent = `Гос. номер: ${auto.gosNumber}`;
+            const invItem = document.createElement('li');
+            invItem.textContent = `Инв. номер: ${auto.invNumber}`;
+            const vinItem = document.createElement('li');
+            vinItem.textContent = `VIN номер: ${auto.vinNumber}`;
+            const releaseItem = document.createElement('li');
+            releaseItem.textContent = `Год выпуска: ${auto.release}`;
+            const garageItem = document.createElement('li');
+            garageItem.textContent = `Номер гаража: ${auto.garageNumber}`;
+            const serviceItem = document.createElement('li');
+            serviceItem.textContent = `Служба: ${auto.service}`;
+            const endItem = document.createElement('li');
+            endItem.textContent = "***************************";
+            resultList.append(modelItem, gosItem, invItem, vinItem, releaseItem, garageItem, serviceItem, endItem);
+        }
+    });
+    if(resultList.innerHTML === ""){
+        alert("Ошибка! Не найдено, проверьте вводимые данные!");
+    }
+}
